e53cb53b636e5f5cd044934face9924941050301
joe <joseph.edwards@itv.com>
2020-12-30 12:27:31 +0000

    not repeating drawings and multiple drawings in place

--- rows/row.py
+++ rows/row.py
@@ -10,15 +10,19 @@ class DrawingRepository:
     @staticmethod
     def add_drawing(story_id, points):
         if story_id is not None:
-            DrawingRepository.drawings[story_id] = points
-            print(DrawingRepository.drawings[story_id])
+            if story_id in DrawingRepository.drawings:
+                DrawingRepository.drawings[story_id].append(points)
+            else:
+                DrawingRepository.drawings[story_id] = [points]
 
     @staticmethod
     def get_drawing(story_id, default=None):
+        #print('getting')
         return DrawingRepository.drawings.get(story_id, default)
 
     @staticmethod
     def has_drawing(story_id):
+        #print('has')
         return story_id in DrawingRepository.drawings
 
 
@@ -26,6 +30,7 @@ class Row(RecycleDataViewBehavior, BoxLayout):
     index = kp.NumericProperty()
     title = kp.StringProperty()
     camera = kp.StringProperty()
+    story_id = kp.StringProperty()
 
     def refresh_view_attrs(self, view, index, data):
         self.index = index  # keep index up to date
@@ -33,91 +38,102 @@ class Row(RecycleDataViewBehavior, BoxLayout):
         super().refresh_view_attrs(view, index, data)
 
     def on_parent(self, instance, parent):
+
         if parent:
             self.ids.index_lbl.text = str(self.index)
             self.ids.title_lbl.text = self.title
             self.ids.camera_lbl.text = self.camera
-            # self.ids.drawingwidget = DrawingRepository.drawings
+            self.ids.drawingwidget.story_id = self.story_id
 
 
 class DrawingWidget(F.RelativeLayout):
-    story_id = F.NumericProperty(None, allownone=True)
-    line_color = F.ColorProperty('#ffffff')
-    line_width = F.NumericProperty(2)
+    story_id = F.StringProperty(None, allownone=True)
     line_points = F.ListProperty()
 
+
+
     def __init__(self, **kwargs):
+
         super().__init__(**kwargs)
-        # Create a Color and Line instruction for this widget. They are
-        # reused throughout the widget lifetime.
-        with self.canvas:
-            self._color = F.Color(self.line_color)
-            self._line = F.Line(width=self.line_width)
-        # Load existing drawing for this story_id, if any
+
+
         if DrawingRepository.has_drawing(self.story_id):
             self.line_points = DrawingRepository.get_drawing(self.story_id)
-        # Immediately draw line if we have any data (could be supplied
-        # in kwargs, or loaded from repository above)
-        if self.line_points:
-            self._line.points = self.line_points
-        # Ensure that canvas instructions are in sync with properties
-        self.bind(line_color=self._update_line_color)
-        self.bind(line_points=self._update_line_points)
-        self.bind(line_width=self._update_line_width)
 
-    def _update_line_color(self, _, color):
-        self._color.rgba = color
+
+        self.bind(line_points=self._update_line_points)
+
+
 
     def _update_line_points(self, _, points):
-        self._line.points = points
 
-    def _update_line_width(self, _, width):
-        self._line.width = width
+        with self.canvas:
+            F.Line(width=2, points=points)
 
-    # This is a property event handler; this method is called automatically
-    # when the story_id property changes. The above functions could be named
-    # `on_line_color`, `on_line_points` and `on_line_width`, and the bind
-    # call in __init__ would not be required. Either technique works. The
-    # downside with this vs bind is that a subclass must call this via
-    # super() in order to implement its own on_story_id method.
+
+
+
+
+
+
     def on_story_id(self, _, story_id):
         drawing = DrawingRepository.get_drawing(story_id)
+
         if drawing is not None:
-            self.line_points = drawing
+            for d in drawing:
+                with self.canvas:
+                    F.Line(width=2, points=d)
         else:
             self.line_points = []
+            self.canvas.clear()
+
+
+
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
-            # If the touch occurs within widget boundaries, we do touch.grab
-            # which ensures that our move/up handlers will always be called
-            # for this touch event's lifetime
+
             touch.grab(self)
-            self.line_points.clear()
-            self.line_points.extend(self.to_local(*touch.pos))
+
+
+
             return True
         return super().on_touch_down(touch)
 
+
+
+
     def on_touch_move(self, touch):
         if touch.grab_current is self:
-            # Our logic only applies to grabbed touches. This test will fail
-            # if the initial touch_down event was outside our boundaries, in
-            # which case the super() handler is called (as usual).
             if self.collide_point(*touch.pos):
+
+
                 self.line_points.extend(self.to_local(*touch.pos))
+
+
+
             return True
         return super().on_touch_move(touch)
 
+
+
+
     def on_touch_up(self, touch):
         if touch.grab_current is self:
-            # Only add the final point if touch is released inside our boundaries.
+
             if self.collide_point(*touch.pos):
                 self.line_points.extend(self.to_local(*touch.pos))
-            # Reject single-point drawings
+
+
             if len(self.line_points) <= 2:
                 self.line_points = []
-            # Store in repository
-            elif self.story_id is not None:
+
+
+            if self.story_id is not None:
                 DrawingRepository.add_drawing(self.story_id, self.line_points[:])
+
+                self.line_points = []
+
+
             return True
         return super().on_touch_up(touch)
