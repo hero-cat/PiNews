e6660e06e82e8181b98099afc5f6926142f1756c
joe <joseph.edwards@itv.com>
2021-02-21 11:49:46 +0000

    popups dunnit, get rid

--- popups/popups.py
+++ popups/popups.py
@@ -27,21 +27,21 @@ class Popups:
     def select_width():
         WidthChoicePencil().open()
 
-    # def show_color_choice_popup(self):
-    #     if not self.color_popup:
-    #         self.color_popup = MDDialog(
-    #             type="custom",
-    #             size_hint_x = None,
-    #             width = "301dp",
-    #             content_cls=ColorChange())
-    #     self.color_popup.open()
-    #
-    # def clear_all_conf_popup(self):
-    #     if not self.clear_all_popup:
-    #         self.clear_all_popup = MDDialog(
-    #             title='Clear all drawings?',
-    #             type="custom",
-    #             size_hint_x=None,
-    #             width="300dp",
-    #             content_cls=ConfBox())
-    #     self.clear_all_popup.open()
+    def show_color_choice_popup(self):
+        if not self.color_popup:
+            self.color_popup = MDDialog(
+                type="custom",
+                size_hint_x = None,
+                width = "301dp",
+                content_cls=ColorChange())
+        self.color_popup.open()
+
+    def clear_all_conf_popup(self):
+        if not self.clear_all_popup:
+            self.clear_all_popup = MDDialog(
+                title='Clear all drawings?',
+                type="custom",
+                size_hint_x=None,
+                width="300dp",
+                content_cls=ConfBox())
+        self.clear_all_popup.open()
